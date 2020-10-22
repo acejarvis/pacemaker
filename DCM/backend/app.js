1242751c4050ad157f66a92d16ee5660d5bd057c
Jarvis Wang <acejarvis98@gmail.com>
2020-10-22 16:14:25 -0400

    updated authentication backend

--- DCM/backend/app.js
+++ DCM/backend/app.js
@@ -3,8 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 const fs = require('fs');
+const cors = require('cors')
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -15,7 +18,7 @@ app.get('/user/list', (req, res) => {
     res.send(users);
 });
 
-app.post('/auth', (req, res) => {
+app.post('/auth/login', (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
     if (username && password) {
@@ -31,6 +34,21 @@ app.post('/auth', (req, res) => {
     }
 })
 
+app.post('/auth/register', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username && password) {
+        const existUsers = getUserData();
+        const findExist = existUsers.find( user => user.username === username )
+        if (!findExist) {
+            return res.send(writeUserData(username, password))
+        }
+        else {
+            return res.json({status: false, msg: 'User exists.'});
+        }
+    }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
@@ -38,4 +56,21 @@ app.listen(port, () => {
 function getUserData () {
     const jsonData = fs.readFileSync('users.json');
     return JSON.parse(jsonData);    
+}
+
+function writeUserData (username, password) {
+    const jsonData = getUserData();
+    const userCount = Object.keys(jsonData).length;
+    console.log(userCount);
+    if (userCount < 10) {
+        jsonData.push({"username": username, "password": password});
+        console.log(jsonData);
+        fs.writeFileSync('users.json', JSON.stringify(jsonData));
+        return { status: true, msg: 'Registered successfully.'};
+    }
+    else {
+        return { status: false, msg: 'User spots are full.'};
+    }
+
+    
 }
